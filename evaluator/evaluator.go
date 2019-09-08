bd0a6ac0bde15ac6b31dfb1735b75a4a7b95062f
devilune <devlune@gmail.com>
2019-09-08 14:30:55 +0900

    4.5.4

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -48,6 +48,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		return &object.Array{Elements: elements}
 
+	// ハッシュリテラル
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
+
 	// 前置式
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
@@ -465,4 +469,36 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
 	}
 
 	return arrayObject.Elements[idx]
+}
+
+/*
+ハッシュリテラルを評価
+*/
+func evalHashLiteral(
+	node *ast.HashLiteral,
+	env *object.Environment,
+) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
