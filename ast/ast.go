bd5477465556aa669c2e000eb6eedf980c0506a2
devilune <devlune@gmail.com>
2019-09-08 14:09:22 +0900

    4.5.2

--- ast/ast.go
+++ ast/ast.go
@@ -223,6 +223,31 @@ func (al *ArrayLiteral) String() string {
 	return out.String()
 }
 
+/*
+ハッシュリテラル
+*/
+type HashLiteral struct {
+	Token token.Token // '{' トークン
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+
 // 前置式
 type PrefixExpression struct {
 	Token    token.Token // 前置トークン、例えば「!」
